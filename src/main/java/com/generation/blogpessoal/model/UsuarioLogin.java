0c3069d4d1f267068562b6388bd0ffebac0cb6db
JoaoPedro <joaopedroflauzinonascimento@gmail.com>
2022-04-07 12:33:45 -0300

    JUnit

--- src/main/java/com/generation/blogpessoal/model/UsuarioLogin.java
+++ src/main/java/com/generation/blogpessoal/model/UsuarioLogin.java
@@ -1,11 +1,11 @@
 package com.generation.blogpessoal.model;
 
 /**
- * A Classe UsuarioLogin não terá nenhuma annotation porque ela
- * não irá gerar uma tabela no Banco de Dados.
+ * A Classe UsuarioLogin não terá nenhuma annotation porque ela não irá gerar
+ * uma tabela no Banco de Dados.
  * 
- * A principal função desta classe é servir de apoio ao processo
- * de login na api.
+ * A principal função desta classe é servir de apoio ao processo de login na
+ * api.
  * 
  */
 public class UsuarioLogin {
@@ -21,6 +21,19 @@ public class UsuarioLogin {
 	private String foto;
 
 	private String token;
+
+	public UsuarioLogin(Long id, String nome, String usuario, String senha, String foto, String token) {
+		this.id = id;
+		this.nome = nome;
+		this.usuario = usuario;
+		this.senha = senha;
+		this.foto = foto;
+		this.token = token;
+	}
+
+	public UsuarioLogin() {
+
+	}
 
 	public Long getId() {
 		return id;
@@ -68,6 +81,6 @@ public class UsuarioLogin {
 
 	public void setFoto(String foto) {
 		this.foto = foto;
-	}	
-	
+	}
+
 }
